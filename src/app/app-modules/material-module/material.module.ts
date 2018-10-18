e728303da2c80f774d7e2fa9630f7caf48a7577d
j-nicholson <johnfnicholson123@gmail.com>
2018-10-18 19:29:14 -0400

    added page header and svg icon logo

--- src/app/app-modules/material-module/material.module.ts
+++ src/app/app-modules/material-module/material.module.ts
@@ -1,15 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { svgIcons } from './icon-map';
 @NgModule({
   imports: [
     CommonModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   declarations: [],
   exports: [
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ]
 })
-export class MaterialModule { }
+export class MaterialModule {
+  constructor(private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer) {
+    svgIcons.forEach(icon => {
+      iconRegistry.addSvgIcon(icon.name, sanitizer.bypassSecurityTrustResourceUrl(icon.path));
+    });
+  }
+}
